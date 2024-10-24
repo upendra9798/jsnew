7e77dbd871b96fc40e314919574c506a4568a07a
upendra9798 <upendrakushwaha022@gmail.com>
2024-10-24 23:15:41 +0530

    acc

--- 01_basics/01_variavle.js
+++ 01_basics/01_variavle.js
@@ -2,6 +2,6 @@ const accountId = 24392
 let accountEmail = "upendr394@google.com"
 var accPass = "3234"
 accountCity = "Jaipur"
-
+let acc = 403940
 accountId=43
-console.log(accountEmail);
+console.log(acc);
