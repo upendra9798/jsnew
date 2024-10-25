f078e2fc7a71d41169c47df031e34f5db563a2bd
upendra9798 <upendrakushwaha022@gmail.com>
2024-10-25 17:19:16 +0530

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score=33
+// let score="33"
+// let score="33abc"
+// let score=null
+// let score=undefined
+// let score=true
+let score= "upendra"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)  //change string to number
+// console.log(typeof valueInNumber )
+// console.log(valueInNumber );
+
+//  "33" => 33
+// "33abc" => NaN  //Can't be change
+// true => 1, false - 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "upendra"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 - true; 0-false
+// "" - false
+// "upendra" - true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
