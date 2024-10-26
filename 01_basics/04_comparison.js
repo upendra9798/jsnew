bd9626b94de5895f644f740fc7ea0a40be1b6449
upendra9798 <upendrakushwaha022@gmail.com>
2024-10-26 16:57:26 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2"> 1)
+// console.log("02" >1)
+
+//Avoid these comparisons
+console.log(null > 0)
+console.log(null == 0) //Equality check
+console.log(null >= 0) //Comparison
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+//=== - strict check
+
+
+console.log("2" === 2);  //It also check data type
