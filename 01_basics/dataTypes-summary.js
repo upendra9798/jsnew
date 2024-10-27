71ec7ab24e17e1f21a47ffa18a11382804ab65fe
upendra9798 <upendrakushwaha022@gmail.com>
2024-10-27 14:35:38 +0530

    Stack and Heap

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -12,12 +12,12 @@ let userEmail;  //undefined
 const id= Symbol('123')
 const anotherID = Symbol('123')
 
-console.log(id === anotherID);
+// console.log(id === anotherID);
 
 const bigNUmber = 928530582305803n
 
 
-//Reference (Non primitive)
+//Call by Reference (Non primitive)
 
 //Array, Objects, Functions
 
@@ -31,16 +31,16 @@ let myObj = {
 //Function
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherID)
-console.log(typeof myFunction) //object function
-console.log(typeof heros)
-console.log(typeof bigNUmber)
-console.log(typeof outsideTemp )
-console.log(typeof userEmail )
-console.log(typeof myObj )
+// console.log(typeof anotherID)
+// console.log(typeof myFunction) //object function
+// console.log(typeof heros)
+// console.log(typeof bigNUmber)
+// console.log(typeof outsideTemp )
+// console.log(typeof userEmail )
+// console.log(typeof myObj )
 
 /*    Data types
 Null - Object
@@ -51,3 +51,27 @@ boolean- boolean
 Number- number
 string - string
 */
+
+
+
+// **********************************************
+
+// Stack(Primitive),  Heap(Non-Primitive)
+
+let myName = "Upendra"
+let anothername = myName
+anothername="Raja"
+
+console.log(myName)   // Not changed as string is call by value(change in copied fn)
+console.log(anothername)
+
+let user1 = {
+    name: "Rohan",
+    Age: 324
+}
+
+let user2= user1
+
+user2.name = "Sapna"
+console.log(user1.name)   //Get changed as function is a call by Reference
+console.log(user2.name)
