ea51e8504c7276d6b1bb5fd0d444a23eaac15bac
upendra9798 <upendrakushwaha022@gmail.com>
2024-12-07 13:08:10 +0530

    Nested scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,9 +15,53 @@ let a =300  //Global scope
 if(true){
     let a=10
 const b=20
-console.log("Inner:", a);
+// console.log("Inner:", a);
 
 } 
 
-console.log(a); 
+// console.log(a); 
 
+
+
+
+//Nested scope (chain of scopes)
+
+function one(){ //parent
+    const username ="hitesh"
+
+    function two(){ //child
+        const website = "youtube"
+        // console.log(username);
+     }
+     //Function Two can use One inside it(Eg-username) but One can't use two inside it(Eg- website)
+//child can use parent but not vice versa
+            // console.log(website);  //Not work(Error)(local scope)
+            two()
+}
+one()
+//First function one executes then two
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website="youtube"
+        // console.log(username+website);
+    }
+    // console.log(website); //Error
+}
+// console.log(username); //Error
+
+// ***************** interesting ***************
+
+// Two ways of function
+
+addone(5) // No error
+function addone(num) {
+    return num+1
+}
+
+//  addTwo(5)  // Error
+const addTwo =function(num){
+    return num+2
+}
