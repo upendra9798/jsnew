6d4d4794a485148b8e1683e88b582ffe56c5b9b2
upendra9798 <upendrakushwaha022@gmail.com>
2024-12-14 12:22:03 +0530

    maps

---
+++ 04_control_flow/07_map.js
@@ -0,0 +1,17 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.map((num) => {return num+10})
+// console.log(newNums);
+
+// CHAINING (USING MORE THAN 1 METHOD)
+
+//  const newNums = myNums
+//                 .map((num) => {return num*10})
+//                 .map((num) => {return num+2})
+// console.log(newNums);
+
+ const newNums = myNums
+                .map((num) => num*10)
+                .map((num) =>  num+2)
+                .filter((num)=> num>50)
+console.log(newNums);
